687fe9f721233b839e90589ea0c8c937c752725f
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2023-10-16 18:00:17 -0300

    segunda parte del desafio

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -1,7 +1,10 @@
+CREATE DATABASE likeme;
+
 CREATE TABLE posts (
-  id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id            VARCHAR(36) NOT NULL,
   titulo        VARCHAR(25) NOT NULL,
   img           VARCHAR(1000) NOT NULL,
   descripcion   VARCHAR(255) NOT NULL,
   likes         INT NULL
+  PRIMARY KEY (id)
 );
--- backend/utils/pg.js
+++ backend/utils/pg.js
@@ -1,27 +1,57 @@
-const express = require('express')
-const cors = require('cors')
-const { Client } = require('pg')
-const app = express()
-const PORT = process.env.PORT ?? 3000
+require('dotenv').config()
+const { Pool } = require('pg')
+const { v4: uuidv4 } = require('uuid')
 
-app.use(cors())
-app.use(express.json())
-
-const db = new Client({
+const config = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   allowExitOnIdle: true
-})
+}
 
-db.connect()
-  .then(() => {
-    console.log('Conexión a PostgreSQL exitosa')
-  })
-  .catch((err) => {
-    console.error('Error al conectar a PostgreSQL:', err)
-  })
+const pool = new Pool(config)
 
-app.listen(PORT, () => console.log(`Server Up in port: ${PORT}`))
+const genericSqlQuery = (query, values) =>
+  pool.query(query, values)
+    .then(({ rows }) => rows)
+    .catch(error => {
+      console.error('Error en la consulta SQL:', error)
+      throw error
+    })
+
+const createPost = async (titulo, img, descripcion, likes) => {
+  const id = uuidv4()
+  const query = 'INSERT INTO posts (id, titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4, $5) RETURNING *'
+  const values = [id, titulo, img, descripcion, likes]
+  return genericSqlQuery(query, values)
+}
+
+const readPosts = async () => {
+  return genericSqlQuery('SELECT * FROM posts')
+}
+
+const readPost = async (id) => {
+  const query = 'SELECT * FROM posts WHERE id = $1'
+  return genericSqlQuery(query, [id])
+}
+
+const updatePost = async (id, { titulo, img, descripcion, likes }) => {
+  const query = 'UPDATE posts SET titulo = $2 WHERE id = 41 RETURNIG *;'
+  const values = [id, titulo, img, descripcion, likes]
+  return await genericSqlQuery(query, values)
+}
+
+const deletePost = async (id) => {
+  const query = 'DELETE FROM posts WHERE id = $1'
+  return genericSqlQuery(query, [id])
+}
+
+module.exports = {
+  createPost,
+  readPosts,
+  readPost,
+  updatePost,
+  deletePost
+}
